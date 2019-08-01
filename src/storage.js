843754adee5658e9f81625a9ab62737a9ca892ad
tudyfilp <tudorf2000@yahoo.com>
2019-08-01 15:45:23 +0300

    deleted some useless code

--- src/storage.js
+++ src/storage.js
@@ -5,23 +5,23 @@ class Storage {
         return JSON.parse(localStorage.getItem(username)) || [];
     }
 
-    static async addNote(username, note) {
-        let usersNotes = await Storage.getUserNotes(username);
+    static addNote(username, note) {
+        let usersNotes = Storage.getUserNotes(username);
         usersNotes.push(note);
         localStorage.setItem(username, JSON.stringify(usersNotes));
     }
 
-    static async deleteNote(username, noteId) {
-        let usersNotes = await Storage.getUserNotes(username);
+    static deleteNote(username, noteId) {
+        let usersNotes = Storage.getUserNotes(username);
 
         let noteIndex = usersNotes.findIndex(note => note.id == noteId);
         usersNotes.splice(noteIndex, 1);
         localStorage.setItem(username, JSON.stringify(usersNotes));
     }
 
-    static async updateNote(username, note) {
+    static updateNote(username, note) {
         const noteId = note.id;
-        let usersNotes = await Storage.getUserNotes(username);
+        let usersNotes = Storage.getUserNotes(username);
         let oldNote = usersNotes.find(nt => nt.id === noteId);
 
         Object.assign(oldNote, note);
